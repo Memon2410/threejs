2d8910bb97db93d614ea09c1c7d07a77f450a83e
Memon2410 <memo2410@gmail.com>
2016-05-05 19:04:22 -0500

    Add click element Mesh

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,7 @@
 function initPage(){
 
 	if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
-		var container, stats;
+		var container;
 		var camera, scene, renderer, objects;
 		var particleLight, pointLight;
 		var dae;
@@ -57,10 +57,6 @@ function initPage(){
 
 
 	function init() {
-		/*
-		var texture = new THREE.Texture( generateTexture() );
-			texture.needsUpdate = true;
-		*/
 		container = document.createElement('div');
 		document.body.appendChild(container);
 		camera = new THREE.PerspectiveCamera(50, containerWidth / containerHeight, 1, 2000);
@@ -72,8 +68,7 @@ function initPage(){
 
 		// Spheres
 		geometry = new THREE.SphereGeometry(0.25, 32, 32);
-		material = new THREE.MeshPhongMaterial({color: 0xff5500, specular: 0x009900, shininess: 30,  map: textureEquirec, transparent: true, opacity: 0.5});
-		//new THREE.MeshBasicMaterial({color: 0xffaa00, transparent: true, blending: THREE.AdditiveBlending, opacity: 0.5});
+		material = new THREE.MeshBasicMaterial({color: 0x0000FF, transparent: true, blending: THREE.AdditiveBlending, opacity: 0.5});
 		sphere1 = new THREE.Mesh( geometry, material);
 		sphere1.position.x = 0.5;
 		sphere1.name = 'sphere-1';
@@ -81,26 +76,14 @@ function initPage(){
 
 		spheresArray.push(sphere1);
 
-		material = new THREE.MeshBasicMaterial({color: 0xffaa00, transparent: true, blending: THREE.AdditiveBlending, opacity: 0.5});
+		material = new THREE.MeshBasicMaterial({color: 0x0000FF, transparent: true, blending: THREE.AdditiveBlending, opacity: 0.5});
 		sphere2 = new THREE.Mesh( geometry, material);
 		sphere2.position.x = -0.7;
 		sphere2.name = 'sphere-2';
 		scene.add(sphere2);
 
 		spheresArray.push(sphere2);
-		/*
-		var radius   = 1,
-			segments = 60,
-			material = new THREE.MeshLambertMaterial({color: 0xdddddd, shading: THREE.FlatShading });
-			geometry = new THREE.CircleGeometry(radius, segments);
-		var circle = new THREE.Line(geometry, material)
 		
-		geometry.vertices.shift();
-		scene.add(circle);
-
-		console.log(': ' + circle)
-		*/
-
 		// Lights
 		scene.add(new THREE.AmbientLight(0xFFFFFF));
 		pointLight = new THREE.PointLight(0xFF3300, 2.5, 50);
@@ -167,38 +150,10 @@ function initPage(){
 		}
 
 
-
-
-
-
-
-
-
 		// Events
 		window.addEventListener('resize', onWindowResize, false);
-		//window.addEventListener('mousemove', onMouseMove, false);
 		window.addEventListener('mousedown', onMouseDown, false);
 	}
-	/*
-	function generateTexture() {
-		var canvas = document.createElement('canvas');
-		canvas.width = 256;
-		canvas.height = 256;
-		var context = canvas.getContext('2d');
-		var image = context.getImageData(0, 0, 256, 256);
-		var x = 0, y = 0;
-		for (var i = 0, j = 0, l = image.data.length; i < l; i += 4, j ++) {
-			x = j % 256;
-			y = x == 0 ? y + 1 : y;
-			image.data[ i ] = 255;
-			image.data[ i + 1 ] = 255;
-			image.data[ i + 2 ] = 255;
-			image.data[ i + 3 ] = Math.floor( x ^ y );
-		}
-		context.putImageData( image, 0, 0 );
-		return canvas;
-	}
-	*/
 	
 	function onWindowResize(e) {
 		containerWidth = window.innerWidth;
@@ -207,39 +162,6 @@ function initPage(){
 		camera.aspect = containerWidth / containerHeight;
 		camera.updateProjectionMatrix();
 		renderer.setSize(containerWidth, containerHeight);
-	}
-
-	function onMouseMove(e) {
-		var mouse3D = new THREE.Vector3( ( event.clientX / containerWidth ) * 2 - 1,   //x
-											-( event.clientY / containerHeight ) * 2 + 1,  //y
-											0.5 );                                        //z
-		mouse3D.unproject(camera);
-		mouse3D.sub(camera.position);                
-		mouse3D.normalize();
-		var raycaster = new THREE.Raycaster(camera.position, mouse3D);
-		var intersects = raycaster.intersectObjects(spheresArray);
-		
-		for( var i = 0; i < intersects.length; i++ ) {
-			var intersection = intersects[ i ],
-			obj = intersection.object;
-			objName = obj.name;
-
-			switch (objName) {
-				case 'sphere-1':
-					TweenMax.to(sphere1.material, 1, {opacity: 1});
-				break;
-				case 'sphere-2':
-					TweenMax.to(sphere2.material, 1, {opacity: 1});
-				break;
-
-				default:
-					TweenMax.to(sphere1.material, 1, {opacity: 0.5});
-					TweenMax.to(sphere2.material, 1, {opacity: 0.5});
-				break;
-			}			
-
-		}
-
 	}
 
 	function onMouseDown( event ) {
@@ -260,24 +182,30 @@ function initPage(){
 			obj = intersection.object;
 			objName = obj.name;
 
-			//var obj1 = scene.getObjectByName( obj.name );
-
 			//TweenMax.to(sphere.material, 2, {opacity: 1});
 			//TweenMax.to(dae.position, 1, {z:6, ease:Expo.easeOut});
 
+			switch (objName) {
+				case 'sphere-1':
+					TweenMax.to(dae.position, 2, {x: camera.position.x, y: camera.position.y - 5.5, z: camera.position.z, ease:Expo.easeOut});
+				break;
+				case 'sphere-2':
+					TweenMax.to(dae.position, 2, {x: camera.position.x, y: camera.position.y - 5.5, z: camera.position.z, ease:Expo.easeOut});
+				break;
 
+			}
 		}
 	}
 
 
-	THREE.Vector3.prototype.pickingRay = function ( camera ) {
-		var tan = Math.tan( 0.5 * THREE.Math.degToRad( camera.fov ) ) / camera.zoom;
+	THREE.Vector3.prototype.pickingRay = function (camera) {
+		var tan = Math.tan(0.5 * THREE.Math.degToRad(camera.fov)) / camera.zoom;
 
 		this.x *= tan * camera.aspect;
 		this.y *= tan; 
 		this.z = - 1;
 
-		return this.transformDirection( camera.matrixWorld );
+		return this.transformDirection(camera.matrixWorld);
 	};
 
 	
